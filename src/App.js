384aa2979d6b11af6486569ddf4ab4d412795edc
darshana balai <darshanakarnavat19@gmail.com>
2022-08-17 22:10:07 +0200

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
-//import first from "./images/first.jpg";
-import diary from "./images/diary.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="Final-Project">
-        <h1>Darshana-Final-Project</h1>
-        <img src={diary} alt="first-image" height={300} width={null} />
+        <h1>Final-Project</h1>
+
         <Navbar />
       </header>
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import "./NavbarStyles.css";
+//import "node_modules/video-react/dist/video-react.css"; // import css
+import video from "../../images/video.mp4";
 
 function Navbar() {
+  const videoEl = useRef(null);
+
+  const attemptPlay = () => {
+    videoEl &&
+      videoEl.current &&
+      videoEl.current.play().catch((error) => {
+        console.error("Error attempting to play", error);
+      });
+  };
+
+  useEffect(() => {
+    attemptPlay();
+  }, []);
   return (
     <>
-      <ul className="Navbar">
-        <li>Home</li>
-        <li>Login</li>
-        <li>Contact</li>
-      </ul>
+      <div>
+        <ul className="Navbar">
+          <li>Home</li>
+          <li>Login</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      <div className="backgroundMain">
+        <video
+          autoPlay
+          playsInline
+          loop
+          id="video"
+          src={video}
+          type="video/mp4"
+          ref={videoEl}
+        ></video>
+      </div>
     </>
   );
 }
